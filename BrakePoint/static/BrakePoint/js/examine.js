1b0bc2c1d634deed516118e875a961edc805af0c
BautistaRalph <bautista.ralphgabriel@gmail.com>
2025-10-15 18:13:38 +0800

    Button positions fixed

--- BrakePoint/static/BrakePoint/js/examine.js
+++ BrakePoint/static/BrakePoint/js/examine.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize map
-    const map = L.map('map').setView([14.5995, 120.9842], 10);
+    const map = L.map('map', {
+        center: [14.5995, 120.9842],
+        zoom: 10,
+        zoomControl: false // Disable default position
+    });
+    L.control.zoom({ position: 'bottomright' }).addTo(map);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
