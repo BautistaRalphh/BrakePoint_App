314f32772202eb97df0f70ddd192d6c7ab3a14e4
julianna-brizuela <julianna_brizuela@dlsu.edu.ph>
2025-10-27 21:44:16 +0800

    More refactoring and compartmentalization of back and frontend.

--- frontend/brakepoint_app/app/logIn/page.tsx
+++ frontend/brakepoint_app/app/logIn/page.tsx
@@ -5,7 +5,7 @@ export default function LogIn() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
