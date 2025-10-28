30c4787c4f07fe3011eb6663824f0378f39e56b7
thisisredtoo <nakamotorika@gmail.com>
2025-10-28 19:45:21 +0800

    Log-In & Sign-Up Created

--- frontend/brakepoint_app/app/signUp/page.tsx
+++ frontend/brakepoint_app/app/signUp/page.tsx
@@ -1,7 +1,91 @@
-import { useState } from "react";
+'use client';
 
-export default function SignUp() {
-  return(
+import { useState } from "react";
+import { Box, Button, TextField, Typography, Paper } from '@mui/material';
+
+export default function SignUpPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  
+
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     
-  )
+    console.log("Submitted:", { username, password });
+  };
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      sx={{ backgroundColor: "#f5f5f5" }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          p: 4,
+          width: 400,
+          borderRadius: 3,
+        }}
+      >
+        <Typography variant="h5" align="center" sx={{ mb:2 }}>
+          <b>Sign Up</b>
+        </Typography>
+
+        <Box component="form" onSubmit={handleSubmit} noValidate>
+          <TextField
+            fullWidth
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+
+          <TextField
+            fullWidth
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            variant="outlined"
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 2, mb: 1 }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+
+        <Typography align="center" variant="body2" sx={{ mt: 2 }}>
+          Already have an account?{" "}
+          <a href="/signUp" style={{ color: "#1976d2", textDecoration: "none" }}>
+            Login here
+          </a>
+        </Typography>
+      </Paper>
+    </Box>
+  );
 }
+
