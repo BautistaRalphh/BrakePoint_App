a495d48f70b0a9bd6aace63a39fd75dea7d76842
julianna-brizuela <julianna_brizuela@dlsu.edu.ph>
2025-10-30 18:35:19 +0800

    Added buttons for polygon and sidebar functionality

--- frontend/brakepoint_app/app/map/page.tsx
+++ frontend/brakepoint_app/app/map/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
+import React, { useState } from 'react';
 import Map from '@components/map/map.js';
+import ToggleDrawer from '@components/map/toggleDrawer';
 
 export default function MapPage() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Map/>
+    <>
+      <ToggleDrawer side="left" open={open}  onToggle={() => setOpen(!open)}></ToggleDrawer>
+      <Map/>
+      
+    </>
+    
   )
-  
-  
+
 }
