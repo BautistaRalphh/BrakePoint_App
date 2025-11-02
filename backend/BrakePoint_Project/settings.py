83b25bedf0b1432470b1364ca270cc48e751f8dc
BautistaRalph <bautista.ralphgabriel@gmail.com>
2025-11-02 22:41:04 +0800

    Structure fix

--- backend/BrakePoint_Project/settings.py
+++ backend/BrakePoint_Project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,12 +51,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # your Next.js dev server
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "BrakePoint_Project.urls"
 
