5ec0ad3f56b3f58c9abec4603f2bb680a25a1da8
BautistaRalph <bautista.ralphgabriel@gmail.com>
2025-11-05 22:59:07 +0800

    Optimize map loading

--- frontend/brakepoint_app/app/layout.tsx
+++ frontend/brakepoint_app/app/layout.tsx
@@ -3,6 +3,10 @@ import Theme from './component_tempo/theme';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://tiles.openfreemap.org" />
+        <link rel="dns-prefetch" href="https://tiles.openfreemap.org" />
+      </head>
       <body>
         <Theme>{children}</Theme>
       </body>
